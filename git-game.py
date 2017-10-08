4865a7ad1cf5df8d18251898e9ff00079402010e
James Ginns <starvagrant@yahoo.com>
2017-10-08 14:26:32 -0500

    Define ANSI escape sequences as constants
    In order to give the program some color, add some predefined ANSI
    sequences for later use.

--- git-game.py
+++ git-game.py
@@ -23,6 +23,14 @@ DANGER = 'danger'
 
 SCREEN_WIDTH = 80
 
+SCREEN_RED = "\033[31m"
+SCREEN_ORANGE = "\033[33m"
+SCREEN_CYAN = "\033[36m"
+SCREEN_GREEN = "\033[32m"
+SCREEN_BLUE = "\033[34m"
+SCREEN_PURPLE = "\033[35m"
+SCREEN_WHITE = "\033[0m"
+
 worldRooms = {
     'Abandoned Treasury': {
     DESC: 'An Abandoned Treasury Lies Before You. The room glitters and is full of treasure chests. Something smells of smoke',
