954f366186a3908dc9a5d6baed0ad02a5ccd60b3
James Ginns <starvagrant@yahoo.com>
2017-10-11 17:33:36 -0500

    Have BaseController read / write json files
    Have the parent of all my controller's be able to read and write
    game data json.

--- BaseController.py
+++ BaseController.py
@@ -2,16 +2,29 @@
 import json
 
 class BaseController:
-    name = "base"
+    def __init__(self):
+       self.name = "base"
+       self.data = {"test": "test"}
+       self.text = ""
+
     def parseJson(text):
         return json.loads(text)
+
     def loadJson(self, data):
         return
 
     def loadJsonFile(self, fileName = 'saved-game/base.json'):
+        with open(fileName, 'r') as f:
+            self.text = f.read()
+            self.data = json.loads(self.text)
         return
 
     def writeJsonFile(self, dirName = 'saved-game'):
+        fileName = dirName + "/" + self.name + ".json"
+        with open(fileName, 'w') as f:
+            f.write(json.dumps(self.data, sort_keys=True,
+                               indent=4, separators=(',',':')))
+            f.close()
         return
 
     def print():
