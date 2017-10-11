e8d1c23aa3c66d58d8f564e5a0d657d47d601599
James Ginns <starvagrant@yahoo.com>
2017-10-11 17:31:35 -0500

    Test BaseController class can read / write json
    The BaseController class is a parent class for every object that
    changes noo-repo game data. It needs to read / write human readable
    json data.

--- unittests.py
+++ unittests.py
@@ -2,17 +2,30 @@
 
 import unittest
 import gitgame
+import json
 from BaseController import BaseController
 
 class MyTests(unittest.TestCase):
     def test_parse_json(self):
         testJson = BaseController.parseJson('{"a":"a","b":"b"}')
-        self.assertEquals(testJson['a'], "a")
-        self.assertEquals(testJson['b'], "b")
+        self.assertEqual(testJson['a'], "a")
+        self.assertEqual(testJson['b'], "b")
     def test_load_json(self):
-        """ Load Mock Data """
+        base = BaseController()
+        base.loadJsonFile('mock-data/base.json')
+        self.assertEqual(base.data['first'], "first")
+        self.assertEqual(base.data['second'], "second")
     def test_write_json(self):
-        """ Write Base Controller's Json to File """
+        base = BaseController()
+        base.loadJsonFile('mock-data/base.json')
+        base.writeJsonFile('mock-data')
+        with open('mock-data/base.json', 'r') as f:
+            text = f.read()
+            testJson = json.loads(text)
+            f.close()
+
+        self.assertEqual(testJson['first'], "first")
+        self.assertEqual(testJson['second'], "second")
 
     def test_write_game(self):
         """ Test games are being property written """
