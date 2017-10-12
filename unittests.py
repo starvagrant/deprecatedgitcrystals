bb6ca79bb337feb6d5a985d82cb9dfd7b9fc34b3
James Ginns <starvagrant@yahoo.com>
2017-10-11 23:40:43 -0500

    Add docstrings to test methods

--- unittests.py
+++ unittests.py
@@ -3,19 +3,24 @@
 import unittest
 import gitgame
 import json
+import os
 from BaseController import BaseController
+from ControllerFactory import ControllerFactory
 
 class MyTests(unittest.TestCase):
     def test_parse_json(self):
+        """ test static method BaseController.parseJson parses Json """
         testJson = BaseController.parseJson('{"a":"a","b":"b"}')
         self.assertEqual(testJson['a'], "a")
         self.assertEqual(testJson['b'], "b")
     def test_load_json(self):
+        """ test loading a json file via BaseController """
         base = BaseController()
         base.loadJsonFile('mock-data/base.json')
         self.assertEqual(base.data['first'], "first")
         self.assertEqual(base.data['second'], "second")
     def test_write_json(self):
+        """ test writing a json file via BaseController """
         base = BaseController()
         base.loadJsonFile('mock-data/base.json')
         base.writeJsonFile('mock-data')
