981123dfab85b9cd2128b17dfe8ce668c0038f1c
James Ginns <starvagrant@yahoo.com>
2017-10-22 22:29:29 -0500

    Add function to print current area
    Add function to print the current area and call it with do_show.
    This allows tests and game play, when "show" is input.

--- gitgame.py
+++ gitgame.py
@@ -17,7 +17,7 @@ SCREEN_WHITE = "\033[0m"
 
 class ExampleCmd(cmd.Cmd):
 
-    prompt = '\n' + SCREEN_ORANGE + 'Git Gems *>' + SCREEN_WHITE
+    prompt = '\n' + SCREEN_ORANGE + 'Git Gems *> ' + SCREEN_WHITE
 
     def default(self, arg):
         print('I do not understand that command. Type "help" for a list of commands.')
@@ -29,9 +29,24 @@ class ExampleCmd(cmd.Cmd):
         self.inventory = controllers['inventory']
         self.characters = controllers['characters']
 
+    def describeLocation(self):
+        location = self.temp.data['location']
+        description = SCREEN_CYAN + '\n' + location + '\n'
+        description += "=" * len(location) + '\n\n' + SCREEN_WHITE
+        description += self.worldRooms.data[location]['desc'] + '\n\n'
+        description += SCREEN_BLUE + "Exits" + '\n'
+        description += "-----" + '\n\n' + SCREEN_WHITE
+        for direction in ('north','east','west','south'):
+            if direction in self.worldRooms.data[location].keys():
+               description += direction.title() + " : " + self.worldRooms.data[location][direction]  + '\n'
+
+        return description
 
     def do_echo(self,arg):
         print(arg.lower())
+
+    def do_show(self,arg):
+        print(self.describeLocation())
 
 
 if __name__ == '__main__':
@@ -43,5 +58,6 @@ if __name__ == '__main__':
     game.loadControllers(controllers)
 
     # Play Game
+    print(game.describeLocation())
     game.cmdloop()
     print("Bye!")
