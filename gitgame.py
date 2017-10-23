d4cc1d17194ab0322ec3326edef42e9ba38e5d56
James Ginns <starvagrant@yahoo.com>
2017-10-22 19:01:16 -0500

    Make game load controllers
    Have the game load the .json based controllers. Consider rebasing
    onto previous controller logic.

--- gitgame.py
+++ gitgame.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+import cmd, textwrap
+from BaseController import *
+from ControllerFactory import *
 
 SCREEN_WIDTH = 80
 
@@ -19,12 +22,25 @@ class ExampleCmd(cmd.Cmd):
     def default(self, arg):
         print('I do not understand that command. Type "help" for a list of commands.')
 
+    def loadControllers(self, controllers):
+        self.alive = controllers['alive']
+        self.temp = controllers['temp']
+        self.worldRooms = controllers['worldRooms']
+        self.inventory = controllers['inventory']
+        self.characters = controllers['characters']
+
 
     def do_echo(self,arg):
         print(arg.lower())
 
 
 if __name__ == '__main__':
+
+    # Initialize Objects, send them to command loop
+    factory = ControllerFactory()
+    controllers = factory.initGame()
+    game = ExampleCmd()
+    game.loadControllers(controllers)
 
     # Play Game
     game.cmdloop()
