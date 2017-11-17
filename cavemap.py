05c601a8bb7e14323b0c5f5911e556feb2656367
James Ginns <starvagrant@yahoo.com>
2017-11-16 23:51:05 -0600

    Add simple cave reference
    Add simple object for keeping track of cave rooms
    to be used for other classes.

---
+++ cavemap.py
@@ -0,0 +1,8 @@
+class Map:
+   def __init__(self):
+        self.rooms = [
+             [ None, "Abandoned Treasury", "Dragon's Lair", None, None],
+             [ "Bottomless Pit", "Alchemist Lab", "Stalagmite Central", None, None],
+             [ "Impressive Caverns", "Wizard's Library", "Git Crystal", "Mine Entrance","Mine"],
+             [None,None,"Mountain Gate","Armory", None]
+            ]
